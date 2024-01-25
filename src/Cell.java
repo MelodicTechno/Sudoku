5750d1f7fa94d8f0441d90e9620575a11267f125
MelodicTechno <617179025@qq.com>
2024-01-25 23:41:38 +0800

    进行uml图的绘制

--- src/Cell.java
+++ src/Cell.java
@@ -19,7 +19,7 @@ public class Cell extends JTextField {
     public static final Color FG_NOT_GIVEN = Color.GRAY;
     public static final Color BG_TO_GUESS  = new Color(253, 219, 145);
     public static final Color BG_CORRECT_GUESS = new Color(198, 245, 198);
-    public static final Color BG_WRONG_GUESS   = new Color(246, 164, 164);
+    public static final Color BG_WRONG_GUESS = new Color(246, 164, 164);
     public static final Font FONT_NUMBERS = new Font("Arial", Font.PLAIN, 28);
 
 
