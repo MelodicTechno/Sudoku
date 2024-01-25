02d1270d2a0a87bd82d6d64342287844b68dbbf5
MelodicTechno <617179025@qq.com>
2024-01-25 12:30:53 +0800

    加粗了一些边框

--- src/Cell.java
+++ src/Cell.java
@@ -1,7 +1,10 @@
 import java.awt.Color;
 import java.awt.Font;
 import java.io.Serial;
-import javax.swing.JTextField;
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
+
 /**
  * Cell类型是用来表示游戏中的每一个单元格，它是Java API中JTextField类型的子类，之所以没有直接使用JTextField类型，就是要在其上进行扩展
  * Cell需要能够输入数字(所以采用JTextField类型作为其父类类型)
@@ -15,8 +18,8 @@ public class Cell extends JTextField {
     public static final Color FG_GIVEN = Color.BLACK;
     public static final Color FG_NOT_GIVEN = Color.GRAY;
     public static final Color BG_TO_GUESS  = new Color(253, 219, 145);
-    public static final Color BG_CORRECT_GUESS = new Color(0, 216, 0);
-    public static final Color BG_WRONG_GUESS   = new Color(216, 0, 0);
+    public static final Color BG_CORRECT_GUESS = new Color(198, 245, 198);
+    public static final Color BG_WRONG_GUESS   = new Color(246, 164, 164);
     public static final Font FONT_NUMBERS = new Font("Arial", Font.PLAIN, 28);
 
 
@@ -33,6 +36,8 @@ public class Cell extends JTextField {
         // 调用JTextField中的方法，用来设置对齐属性以及字体
         super.setHorizontalAlignment(JTextField.CENTER);
         super.setFont(FONT_NUMBERS);
+        super.setBorder(new LineBorder(Color.GRAY, 1));
+        makeBorder();
     }
 
     /** 当重新一局游戏时，通过这个方法设定单元格中正确的数字和初始状态 */
@@ -59,6 +64,33 @@ public class Cell extends JTextField {
             super.setBackground(BG_CORRECT_GUESS);
         } else if (status == CellStatus.WRONG_GUESS) {
             super.setBackground(BG_WRONG_GUESS);
+        }
+    }
+    public void makeBorder() {
+        // Border oldBorder = super.getBorder();
+        Border rightBorder = BorderFactory.createMatteBorder(0, 0, 0, 1, Color.BLACK);
+        Border topBorder = BorderFactory.createMatteBorder(1, 0, 0, 0, Color.BLACK);
+        Border bottomBorder = BorderFactory.createMatteBorder(0, 0, 1, 0, Color.BLACK);
+        Border leftBorder = BorderFactory.createMatteBorder(0, 1, 0, 0, Color.BLACK);
+        if (row % 3 == 0) {
+            Border oldBorder = super.getBorder();
+            Border newTopBorder = BorderFactory.createCompoundBorder(topBorder, oldBorder);
+            super.setBorder(newTopBorder);
+        }
+        if (col % 3 == 0) {
+            Border oldBorder = super.getBorder();
+            Border newTopBorder = BorderFactory.createCompoundBorder(leftBorder, oldBorder);
+            super.setBorder(newTopBorder);
+        }
+        if (col == 8) {
+            Border oldBorder = super.getBorder();
+            Border newRightBorder = BorderFactory.createCompoundBorder(rightBorder, oldBorder);
+            super.setBorder(newRightBorder);
+        }
+        if (row == 8) {
+            Border oldBorder = super.getBorder();
+            Border newBottomBorder = BorderFactory.createCompoundBorder(bottomBorder, oldBorder);
+            super.setBorder(newBottomBorder);
         }
     }
 }
